be21796159877c07761d142ba7dd5a0931d1c166
vpqxl1 <vpqxl69@gmail.com>
2025-08-30 21:29:34 +0200

    Update config.js

--- config.js
+++ config.js
@@ -2,5 +2,6 @@
 module.exports = {
     token: "YOUR_TOKEN_HERE",
     prefix: "PREFIX",
-    allowedUserIDs: ['userid1', 'userid2', 'userid3']
+    allowedUserIDs: ['userid1', 'userid2', 'userid3'],
+    nasaApiKey: "NASAKEY" // Using DEMO_KEY for NASA API (only needed for apod)
 };
