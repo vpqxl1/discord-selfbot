14c34d11f30be1852651fc5cf77578b099e9bb48
vpqxl1 <vpqxl69@gmail.com>
2025-08-30 21:22:46 +0200

    Delete commands directory

--- commands/coinflip.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    name: 'coinflip',
-    description: 'Flips a coin.',
-    execute(channel, message, client, args) {
-        const result = Math.random() < 0.5 ? 'Heads' : 'Tails';
-        message.channel.send(`🪙 The coin landed on **${result}**!`);
-    }
-};
