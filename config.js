0060f8807d2065fcef021033cd53319db64d8e3b
diskspoof <vpqxl69@gmail.com>
2026-02-09 09:40:58 +0100

    Add ltcAdress to config.js

--- config.js
+++ config.js
@@ -3,5 +3,6 @@ module.exports = {
     token: "YOUR_TOKEN_HERE",
     prefix: "PREFIX",
     allowedUserIDs: ['userid1', 'userid2', 'userid3'],
-    nasaApiKey: "DEMO_KEY" // Works for 30 requests/h and 50 requests/day (per IP), should be fine for APOD
+    nasaApiKey: "DEMO_KEY", // Works for 30 requests/h and 50 requests/day (per IP), should be fine for APOD
+    ltcAdress: "Addy"
 };
