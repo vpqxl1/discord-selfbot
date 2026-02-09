e173ba24556df716b9b95e1b2e0bc4af1c2aa4eb
diskspoof <vpqxl69@gmail.com>
2026-02-09 09:01:06 +0100

    Update NASA API key to use DEMO_KEY

--- config.js
+++ config.js
@@ -3,5 +3,5 @@ module.exports = {
     token: "YOUR_TOKEN_HERE",
     prefix: "PREFIX",
     allowedUserIDs: ['userid1', 'userid2', 'userid3'],
-    nasaApiKey: "NASAKEY" // Using DEMO_KEY for NASA API (only needed for apod)
+    nasaApiKey: "DEMO_KEY" // Works for 30 requests/h and 50 requests/day (per IP), should be fine for APOD
 };
