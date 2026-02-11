ebf5f69ebb2f77ddcf0dec86de6692ce4fa76d24
diskspoof <vpqxl69@gmail.com>
2026-02-11 18:52:21 +0100

    Add silentPermissionDenied to config

--- config.js
+++ config.js
@@ -4,5 +4,6 @@ module.exports = {
     prefix: "PREFIX",
     allowedUserIDs: ['userid1', 'userid2', 'userid3'],
     nasaApiKey: "DEMO_KEY", // Works for 30 requests/h and 50 requests/day (per IP), should be fine for APOD
-    ltcAdress: "Addy"
+    ltcAdress: "Addy",
+  silentPermissionDenied: "true"
 };
